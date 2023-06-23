dd1e7a2e3ba4712da41938c215414df0320bc30e
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-06-23 13:37:21 +0100

    update-img

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-
-const endorsementsTxt = document.getElementById("text-el");
-const fromEl = document.getElementById("from");
-const toEl = document.getElementById("to");
-const formEl = document.getElementById("form-el");
-const endorsementsContainer = document.getElementById("endorsements-container");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const endorsementsTxt = document.getElementById("text-el")
+const fromEl = document.getElementById("from")
+const toEl = document.getElementById("to")
+const formEl = document.getElementById("form-el")
+const endorsementsContainer = document.getElementById("endorsements-container")
 
 const appSettings = {
   databaseURL: "https://champion-6a054-default-rtdb.firebaseio.com/",
-};
+}
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const dataInDatabase = ref(database, "champion");
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const dataInDatabase = ref(database, "champion")
+const heartIcon = "/assets/heart.png"
+const heartIconPurple = "/assets/heart-purple.png"
 
 formEl.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const textAreaValue = endorsementsTxt.value;
-  const fromValue = fromEl.value;
-  const toValue = toEl.value;
+  e.preventDefault()
+  const textAreaValue = endorsementsTxt.value
+  const fromValue = fromEl.value
+  const toValue = toEl.value
 
   push(dataInDatabase, {
     textareaValue: textAreaValue,
@@ -27,46 +29,46 @@ formEl.addEventListener("submit", (e) => {
     toValue: toValue,
     likeCount: 0,
     likeId: Date.now().toString(),
-  });
-  clearInputsFields();
-});
+  })
+  clearInputsFields()
+})
 
 function likeEndorsements(likeId) {
-  const countLikeElement = document.getElementById(`count-like-${likeId}`);
-  const countLikeValue = parseInt(countLikeElement.textContent);
+  const countLikeElement = document.getElementById(`count-like-${likeId}`)
+  const countLikeValue = parseInt(countLikeElement.textContent)
 
-  const isLiked = countLikeElement.classList.contains("liked");
-  const newLikeCount = isLiked ? countLikeValue - 1 : countLikeValue + 1;
+  const isLiked = countLikeElement.classList.contains("liked")
+  const newLikeCount = isLiked ? countLikeValue - 1 : countLikeValue + 1
 
-  countLikeElement.textContent = newLikeCount;
+  countLikeElement.textContent = newLikeCount
 
-  const endorsementRef = ref(database, `champion/${likeId}`);
+  const endorsementRef = ref(database, `champion/${likeId}`)
   update(endorsementRef, {
     likeCount: newLikeCount,
     isLiked: !isLiked,
-  });
+  })
 }
 
 endorsementsContainer.addEventListener("click", (event) => {
-  const target = event.target;
+  const target = event.target
 
   if (target.classList.contains("heart-container") || target.closest(".heart-container")) {
-    const heartContainer = target.closest(".heart-container");
-    const likeId = heartContainer.id.replace("toggle-like-", "");
-    likeEndorsements(likeId);
+    const heartContainer = target.closest(".heart-container")
+    const likeId = heartContainer.id.replace("toggle-like-", "")
+    likeEndorsements(likeId)
   }
-});
+})
 
 onValue(dataInDatabase, (snapshot) => {
-  let endorsementsContainerHtml = "";
+  let endorsementsContainerHtml = ""
   snapshot.forEach((childSnapshot) => {
-    const itemInDatabase = childSnapshot.val();
-    const likeId = childSnapshot.key;
+    const itemInDatabase = childSnapshot.val()
+    const likeId = childSnapshot.key
 
-    const likeCount = itemInDatabase.likeCount || 0;
-    const isLiked = itemInDatabase.isLiked || false;
+    const likeCount = itemInDatabase.likeCount || 0
+    const isLiked = itemInDatabase.isLiked || false
 
-    const heartImgSrc = isLiked ? "/assets/heart-purple.png" : "/assets/heart.png";
+    const heartImgSrc = isLiked ? heartIconPurple : heartIcon
 
     endorsementsContainerHtml += `
       <div class="endorsements-html">
@@ -80,14 +82,14 @@ onValue(dataInDatabase, (snapshot) => {
           </div>
         </div>
       </div>
-    `;
-  });
+    `
+  })
 
-  endorsementsContainer.innerHTML = endorsementsContainerHtml;
-});
+  endorsementsContainer.innerHTML = endorsementsContainerHtml
+})
 
 function clearInputsFields() {
-  endorsementsTxt.value = "";
-  fromEl.value = "";
-  toEl.value = "";
+  endorsementsTxt.value = ""
+  fromEl.value = ""
+  toEl.value = ""
 }

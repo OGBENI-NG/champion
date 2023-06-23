1affe15af12d21d7ed557a60ed63a8e94a93ed68
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-06-23 22:15:33 +0100

    final

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const endorsementsContainer = document.getElementById("endorsements-container")
 const appSettings = {
   databaseURL: "https://champion-6a054-default-rtdb.firebaseio.com/"
 }
-
+//setting up firebase
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const dataInDatabase = ref(database, "champion")
@@ -18,85 +18,85 @@ const heartIcon = "/assets/heart.png"
 const heartIconPurple = "/assets/heart-purple.png"
 
 formEl.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const textAreaValue = endorsementsTxt.value
-  const fromValue = fromEl.value
-  const toValue = toEl.value
-
-  push(dataInDatabase, {
-    textareaValue: textAreaValue,
-    fromValue: fromValue,
-    toValue: toValue,
-    likeCount: 7,
-    likeId: Date.now().toString()
-  })
-  clearInputsFields()
+    e.preventDefault()
+    const textAreaValue = endorsementsTxt.value
+    const fromValue = fromEl.value
+    const toValue = toEl.value
+    // pushing all input values to firebase as abject
+    push(dataInDatabase, {
+        textareaValue: textAreaValue,
+        fromValue: fromValue,
+        toValue: toValue,
+        likeCount: 7,
+        likeId: Date.now().toString()
+    })
+    clearInputsFields()
 })
-
+//creating endorsements field from innerHTML
 function createEndorsementHTML(endorsement) {
-  const { toValue, textareaValue, fromValue, likeCount, isLiked, likeId } = endorsement
-  const heartImgSrc = isLiked ? heartIconPurple : heartIcon
+    const { toValue, textareaValue, fromValue, likeCount, isLiked, likeId } = endorsement
+    const heartImgSrc = isLiked ? heartIconPurple : heartIcon
 
-  return `
-    <div class="endorsements-html">
-      <h3>To ${toValue}</h3>
-      <p>${textareaValue}</p>
-      <div class="from-container">
-        <h3>From ${fromValue}</h3>
-        <div class="heart-container" id="toggle-like-${likeId}">
-          <img src="${heartImgSrc}" alt="heart-icon" class="heart">
-          <span id="count-like-${likeId}" ${isLiked ? 'class="liked"' : ""}>${likeCount}</span>
+    return `
+        <div class="endorsements-html">
+        <h3>To ${toValue}</h3>
+        <p>${textareaValue}</p>
+        <div class="from-container">
+            <h3>From ${fromValue}</h3>
+            <div class="heart-container" id="toggle-like-${likeId}">
+            <img src="${heartImgSrc}" alt="heart-icon" class="heart">
+            <span id="count-like-${likeId}" ${isLiked ? 'class="liked"' : ""}>${likeCount}</span>
+            </div>
         </div>
-      </div>
-    </div>
-  `
+        </div>
+    `
 }
 
 function renderEndorsements(endorsements) {
-  const endorsementsHtml = endorsements.map(createEndorsementHTML).join("")
-  endorsementsContainer.innerHTML = endorsementsHtml
+    const endorsementsHtml = endorsements.map(createEndorsementHTML).join("")
+    endorsementsContainer.innerHTML = endorsementsHtml
 }
 
 function likeEndorsements(likeId) {
-  const countLikeElement = document.getElementById(`count-like-${likeId}`)
-  const countLikeValue = parseInt(countLikeElement.textContent)
+    const countLikeElement = document.getElementById(`count-like-${likeId}`)
+    const countLikeValue = parseInt(countLikeElement.textContent)
 
-  const isLiked = countLikeElement.classList.contains("liked")
-  const newLikeCount = isLiked ? countLikeValue - 1 : countLikeValue + 1
+    const isLiked = countLikeElement.classList.contains("liked")
+    const newLikeCount = isLiked ? countLikeValue - 1 : countLikeValue + 1
 
-  countLikeElement.textContent = newLikeCount
+    countLikeElement.textContent = newLikeCount
 
-  const endorsementRef = ref(database, `champion/${likeId}`)
-  update(endorsementRef, {
-    likeCount: newLikeCount,
-    isLiked: !isLiked
-  })
+    const endorsementRef = ref(database, `champion/${likeId}`)
+    update(endorsementRef, {
+        likeCount: newLikeCount,
+        isLiked: !isLiked
+    })
 }
 
 endorsementsContainer.addEventListener("click", (event) => {
-  const target = event.target
+    const target = event.target
 
-  if (target.classList.contains("heart-container") || target.closest(".heart")) {
-    const heartContainer = target.closest(".heart-container")
-    const likeId = heartContainer.id.replace("toggle-like-", "")
-    likeEndorsements(likeId)
-  }
+    if (target.classList.contains("heart-container") || target.closest(".heart")) {
+        const heartContainer = target.closest(".heart-container")
+        const likeId = heartContainer.id.replace("toggle-like-", "")
+        likeEndorsements(likeId)
+    }
 })
 
 onValue(dataInDatabase, (snapshot) => {
-  const endorsements = []
-  snapshot.forEach((childSnapshot) => {
-    const itemInDatabase = childSnapshot.val()
-    const likeId = childSnapshot.key
-    itemInDatabase.likeId = likeId
-    endorsements.unshift(itemInDatabase)
-  })
+    const endorsements = []
+    snapshot.forEach((childSnapshot) => {
+        const itemInDatabase = childSnapshot.val()
+        const likeId = childSnapshot.key
+        itemInDatabase.likeId = likeId
+        endorsements.unshift(itemInDatabase)
+    })
 
-  renderEndorsements(endorsements)
+    renderEndorsements(endorsements)
 })
 
 function clearInputsFields() {
-  endorsementsTxt.value = ""
-  fromEl.value = ""
-  toEl.value = ""
+    endorsementsTxt.value = ""
+    fromEl.value = ""
+    toEl.value = ""
 }
